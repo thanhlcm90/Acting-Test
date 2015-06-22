2510c5217e3d9f161843d11eff72d6b011a480ac
Daniel Le <thanhlcm90@gmail.com>
2015-06-22 10:37:29 +0700

    Fix, upload image to cloudinary

--- app/controllers/upload.server.controller.js
+++ app/controllers/upload.server.controller.js
@@ -3,15 +3,17 @@
 /**
  * Module dependencies.
  */
-var mongoose = require('mongoose'),
-    fs = require('fs'),
-    path = require('path'),
-    formidable = require('formidable');
+var mongoose = require('mongoose');
+var cloudinary = require('cloudinary');
+var fs = require('fs');
+var path = require('path');
+var formidable = require('formidable');
 
 exports.upload = function(req, res) {
     var form = new formidable.IncomingForm();
     var uploadDir = path.normalize(__dirname + '/../..') + "/public/uploads";
     form.uploadDir = uploadDir;
+    form.keepExtensions = true;
 
     form.parse(req, function(err, fields, files) {
         var html;
@@ -21,14 +23,20 @@ exports.upload = function(req, res) {
         }
         var fileName = mongoose.Types.ObjectId() + '_' + files.upload.name;
         var dest = uploadDir + '/' + fileName;
-        fs.rename(files.upload.path, dest, function(err) {
-            if (err) {
-                console.log(err);
-                return;
+
+        // upload image to cloudinary
+        cloudinary.uploader.upload(files.upload.path, function(image) {
+            var msg = 'Uploaded file successfully';
+            var url = '';
+            if (!image) {
+                console.error('Upload failed');
+                msg = 'Uploaded file failed. Please try again later!';
+            } else {
+                url = image.url;
             }
             html = '';
             html += '<script type="text/javascript">';
-            html += '   window.parent.CKEDITOR.tools.callFunction(' + req.query.CKEditorFuncNum + ', "/uploads/' + fileName + '", "Uploaded file successfully");';
+            html += '   window.parent.CKEDITOR.tools.callFunction(' + req.query.CKEditorFuncNum + ', "' + url + '", "' + msg + '");';
             html += '</script>';
             res.send(html);
         });
