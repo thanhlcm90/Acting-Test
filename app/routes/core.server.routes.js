eb9c030ed4d1298f7f2a2f190ce41fd073ded799
anhpt <tuananhqn93@gmail.com>
2015-10-06 08:49:42 +0700

    Frontend

--- app/routes/core.server.routes.js
+++ app/routes/core.server.routes.js
@@ -5,4 +5,11 @@ module.exports = function(app) {
 	var core = require('../../app/controllers/core.server.controller');
 	app.route('/').get(core.index);
 	app.route('/admin').get(core.admin);
+	app.route('/home').get(core.home);
+	app.route('/about').get(core.about);
+	app.route('/cases').get(core.cases);
+	app.route('/tutorial').get(core.tutorial);
+	app.route('/forum').get(core.forum);
+	app.route('/find').get(core.find);
+	app.route('/profile').get(core.profile);
 };
